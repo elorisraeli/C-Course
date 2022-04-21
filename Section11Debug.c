dce4f71c430254fc6f837a8e09fcaee37e2d9e1f
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2022-04-21 17:48:42 +0300

    Section11 - Debugging

---
+++ Section11Debug.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    // double n1, n2, n3;
+
+    // printf("Enter 3 numbers: ");
+    // scanf("%lf %lf %lf", &n1, &n2, &n3);
+
+    // double average = (n1+n2+n3)/3;
+    // printf("The average is: %lf", average);
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("The index is: %d\n", i);
+    }
+    
+
+    return 0;
+}
