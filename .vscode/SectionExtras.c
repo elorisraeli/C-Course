b997bdd2c84033b860cd8116dd363677371df51e
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2022-05-05 12:36:57 +0300

    Section Exrtas and Summery

---
+++ .vscode/SectionExtras.c
@@ -0,0 +1,49 @@
+/*
+
+More Thing To Learn:  (Expert in C language)
+- more on data types: define your own types (typedef)
+- more on the preprocessor: string concatenation
+- more on void*
+- static libararies and shared object
+- macros
+- unions
+- fucntion pointer (big topic)
+- advanced pointers (pointers to pointers)
+- variable arguments to function (variadic functions)
+- dynamic linking (dlm_open)
+- signals, forking and inter-process communication
+- threading and concurrency (time of executing code lines)
+- sockets (connection to other computers)
+- setjump and longjump for restoring state
+- more on memory managment and fragmantation
+- more on making your program portable
+- interfacing with kernel modules (drivers and ioctls)
+- more on compiler and linker flags
+- advanced use on debagger (gdb)
+- profiling and tracing tools (gprof, dtrace, strace)
+- memory debuging tools such as valgrid
+
+
+
+Summery of the course:
+- Basic Operators: logical, arithmetic and assignment
+- Conditional Statements: making decisions(if, switch)
+- Repeating code: looping (for, while, do-while)
+- Arrays: defining and initializing, multi-dimensional
+- Functions: declaration and use, arguments and parameters, call by value vs. call by reference
+- Debugging: call stack, mistakes, understanding compiler messages
+- Structs: initialazing, nested structures
+- Character Strings: basics, arrays of chars, character operations
+THE MOST IMPORTENT SUBJECT IN C: POINTERS
+- Pointers: definition and use, using with functions and arrays, malloc, pointer arithmetic
+- Preprocessor: #define, #include
+- Input and Output: command line arguments, scanf, printf
+- File Input/Output: reaing and writing to a file, fgets, fputs, fgetc, fseek, etc.
+- Standard C Library: string functions, math functions, utility functions, standard header files
+
+*/
+
+int main(){
+    printf("CONGRATULATIONS!!!!!!!");
+    return 0;
+}
